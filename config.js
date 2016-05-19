8251bddbd6550eaed7d227c2665478d96f197e13
Darma Kurniawan Harefa <darmakurniawanharefa@gmail.com>
2016-05-20 00:31:51 +0700

    Tambah file config.js untuk menyimpan configurasi aplikasi

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	consumer_key: 'RgKVGIpeM38nvAljHmOIcZgu2',
+	consumer_secret: 'cX1oxhS9veOpwPnnECdba62FsOy0LGsNHl7OEtTJz4GYCx8SNX',
+	access_token_key: '1688905356-8RHE2C6YJSvKplnNyykbMHgcXzaKcqqSll1QLkt',
+	access_token_secret:'w9LQCQuAEWQn81KwAOhDqeUyn3I4EmhSrSkf2k2UulpZy'
+}
